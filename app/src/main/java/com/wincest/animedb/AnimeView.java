6eec7924ce2211e98442311c684a867e5b6e4269
VerticalFarming <coreanonuclear@gmail.com>
2019-01-31 12:31:48 +0100

    Agregado adapter para el ListView, creado objeto que representa un anime. Falta cargar la informacion desde la BD.

--- app/src/main/java/com/wincest/animedb/AnimeView.java
+++ app/src/main/java/com/wincest/animedb/AnimeView.java
@@ -10,16 +10,6 @@ public class AnimeView extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.anime_list);
-
-        //TODO cargar todos los datos de la ficha tecnica
-
-        //TODO poner un ImageView para visualizar la portada o imagen de la serie
-
-        //TODO forma para volver a la pagina anterior
-
-        //TODO boton para añadir en favoritos (una estrellita que se colorea cuando esta en favoritos)
-
-        //TODO cambiar layout de la pagina para un Grid, hacerlo mas bonito
+        setContentView(R.layout.anime_view);
     }
 }
